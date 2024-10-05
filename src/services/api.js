15feba731f462c3f4b7a9265d1cd3f20ff3d523b
unknown <raidislam>
2024-10-06 02:21:01 +0600

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = 'http://116.193.222.198:8090';
+const API_BASE_URL = "http://116.193.222.198:8090";
 
 const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const getMembers = () => {
-    return api.get('/members');
+  return api.get("api/members");
 };
