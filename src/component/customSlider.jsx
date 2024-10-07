092f174dd78e5fa866494251fa9df77bb1f31920
raidislam <raidislam@gmail.com>
2024-10-07 12:41:46 +0600

    update

--- src/component/customSlider.jsx
+++ src/component/customSlider.jsx
@@ -1,40 +1,39 @@
-import { useEffect, useState } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
-import { getMembers } from "../services/api";
-import Card from "./Card";
+import { useEffect, useState } from "react"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css"
+import { getMembers } from "../services/api"
+import Card from "./Card"
 
 export default function CustomSlider() {
-  const [slides, setSlides] = useState([]);
-  const [notice, setNotice] = useState([]);
-  const [tvcList, setTvcList] = useState([]);
-  const [currentTitle, setCurrentTitle] = useState("");
-  const [companyTitle, setCompanyTitle] = useState("");
-  const [showFullImage, setShowFullImage] = useState(true);
+  const [slides, setSlides] = useState([])
+  const [notice, setNotice] = useState([])
+  const [tvcList, setTvcList] = useState([])
+  const [currentTitle, setCurrentTitle] = useState("")
+  const [companyTitle, setCompanyTitle] = useState("")
+  const [showFullImage, setShowFullImage] = useState(true)
 
   useEffect(() => {
     // Fetch users when the component mounts
     getMembers()
       .then((response) => {
-        setSlides(response.data.slides);
-        setNotice(response.data.notices);
-        setTvcList(response.data.custom_screens);
+        setSlides(response.data.slides)
+        setNotice(response.data.notices)
+        setTvcList(response.data.custom_screens)
 
-        setCurrentTitle(slides[0]?.company_name);
-        setCompanyTitle(slides[0]?.title);
-
+        setCurrentTitle(slides[0]?.company_name)
+        setCompanyTitle(slides[0]?.title)
       })
-      .catch((error) => console.error("Error fetching users:", error));
-  }, [showFullImage]);
+      .catch((error) => console.error("Error fetching users:", error))
+  }, [showFullImage])
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setShowFullImage(false);
-    }, 1000);
+      setShowFullImage(false)
+    }, 1000)
 
-    return () => clearTimeout(timer);
-  }, []);
+    return () => clearTimeout(timer)
+  }, [])
 
   const settings = {
     dots: false,
@@ -46,14 +45,14 @@ export default function CustomSlider() {
     autoplay: true,
     autoplaySpeed: 800,
     beforeChange: (oldIndex, newIndex) => {
-      setCurrentTitle(slides[newIndex].company_name);
-      setCompanyTitle(slides[newIndex].title);
+      setCurrentTitle(slides[newIndex].company_name)
+      setCompanyTitle(slides[newIndex].title)
     },
     draggable: false,
-  };
+  }
 
   const viddeoSettings = {
-    dots: true, // Show dots for each slide
+    dots: false, // Show dots for each slide
     fade: true, // Enable fade effect between slides
     infinite: true, // Enable infinite scrolling of slides
     speed: 500, // Slide transition speed (0.5s)
@@ -64,7 +63,7 @@ export default function CustomSlider() {
     pauseOnHover: false, // Don't stop autoplay when hovering over the slider
     waitForAnimate: false, // Don't wait for animation to finish before next slide
     arrows: false, // Hide navigation arrows if not needed
-  };
+  }
 
   return (
     <div className="">
@@ -97,7 +96,10 @@ export default function CustomSlider() {
             <div className="h-full py-8">
               <Slider {...settings}>
                 {slides?.map((slide, index) => (
-                  <div key={index} className="h-full flex justify-center items-center" >
+                  <div
+                    key={index}
+                    className="h-full flex justify-center items-center"
+                  >
                     {/*<div className="grid grid-cols-3 gap-2 justify-center divosvai">
                       {slide.cardsContent.map((card, cardIndex) => (
                         <div key={cardIndex} className="w-full flex justify-center items-center">
@@ -106,67 +108,92 @@ export default function CustomSlider() {
                       ))}
                     </div>*/}
                     {slide.cardsContent.length === 6 && (
-                        <div className="grid grid-cols-3 gap-1 justify-center">
-                          {slide.cardsContent.map((card, cardIndex) => (
-                              <div key={cardIndex} className="w-full flex justify-center items-center">
-                                <Card card={card} />
-                              </div>
-                          ))}
-                        </div>
+                      <div className="grid grid-cols-3 gap-1 justify-center">
+                        {slide.cardsContent.map((card, cardIndex) => (
+                          <div
+                            key={cardIndex}
+                            className="w-full flex justify-center items-center"
+                          >
+                            <Card card={card} />
+                          </div>
+                        ))}
+                      </div>
                     )}
                     {slide.cardsContent.length === 5 && (
-                        <>
-                          <div className="grid grid-cols-3 gap-1 justify-center">
-                            {slide.cardsContent.slice(0, 3).map((card, cardIndex) => (
-                                <div key={cardIndex} className="w-full flex justify-center items-center">
-                                  <Card card={card} />
-                                </div>
+                      <>
+                        <div className="grid grid-cols-3 gap-1 justify-center">
+                          {slide.cardsContent
+                            .slice(0, 3)
+                            .map((card, cardIndex) => (
+                              <div
+                                key={cardIndex}
+                                className="w-full flex justify-center items-center"
+                              >
+                                <Card card={card} />
+                              </div>
                             ))}
-                          </div>
-                          <div className="grid grid-cols-2 gap-1 justify-center mt-1">
-                            {slide.cardsContent.slice(3).map((card, cardIndex) => (
-                                <div key={cardIndex} className="w-full flex justify-center items-center">
-                                  <Card card={card} />
-                                </div>
+                        </div>
+                        <div className="grid grid-cols-2 gap-1 justify-center mt-1">
+                          {slide.cardsContent
+                            .slice(3)
+                            .map((card, cardIndex) => (
+                              <div
+                                key={cardIndex}
+                                className="w-full flex justify-center items-center"
+                              >
+                                <Card card={card} />
+                              </div>
                             ))}
-                          </div>
-                        </>
+                        </div>
+                      </>
                     )}
                     {slide.cardsContent.length === 4 && (
-                        <div className="grid grid-cols-2 gap-1 justify-center">
-                          {slide.cardsContent.map((card, cardIndex) => (
-                              <div key={cardIndex} className="w-full flex justify-center items-center">
-                                <Card card={card} />
-                              </div>
-                          ))}
-                        </div>
+                      <div className="grid grid-cols-2 gap-1 justify-center">
+                        {slide.cardsContent.map((card, cardIndex) => (
+                          <div
+                            key={cardIndex}
+                            className="w-full flex justify-center items-center"
+                          >
+                            <Card card={card} />
+                          </div>
+                        ))}
+                      </div>
                     )}
                     {slide.cardsContent.length === 3 && (
-                        <div className="grid grid-cols-3 gap-1 justify-center">
-                          {slide.cardsContent.map((card, cardIndex) => (
-                              <div key={cardIndex} className="w-full flex justify-center items-center">
-                                <Card card={card} />
-                              </div>
-                          ))}
-                        </div>
+                      <div className="grid grid-cols-3 gap-1 justify-center">
+                        {slide.cardsContent.map((card, cardIndex) => (
+                          <div
+                            key={cardIndex}
+                            className="w-full flex justify-center items-center"
+                          >
+                            <Card card={card} />
+                          </div>
+                        ))}
+                      </div>
                     )}
                     {slide.cardsContent.length === 2 && (
-                        <div className="grid grid-cols-2 gap-1 justify-center">
-                          {slide.cardsContent.map((card, cardIndex) => (
-                              <div key={cardIndex} className="w-full flex justify-center items-center">
-                                <Card card={card} />
-                              </div>
-                          ))}
-                        </div>
+                      <div className="grid grid-cols-2 gap-1 justify-center">
+                        {slide.cardsContent.map((card, cardIndex) => (
+                          <div
+                            key={cardIndex}
+                            className="w-full flex justify-center items-center"
+                          >
+                            <Card card={card} />
+                          </div>
+                        ))}
+                      </div>
                     )}
                     {slide.cardsContent.length === 1 && (
-                        <div className="grid grid-cols-1 gap-1 justify-center">
-                          {slide.cardsContent.map((card, cardIndex) => (
-                              <div key={cardIndex} className="w-full flex justify-center items-center">
-                                <Card card={card} />
-                              </div>
-                          ))}
-                        </div>
+                      <div className="grid grid-cols-1 gap-1 justify-center">
+                        {slide.cardsContent.map((card, cardIndex) => (
+                          <div
+                            key={cardIndex}
+                            className="w-full flex justify-center items-center"
+                          >
+                            <Card card={card} />
+                          </div>
+                        ))}
+                      </div>
                     )}
                   </div>
                 ))}
@@ -175,25 +202,12 @@ export default function CustomSlider() {
 
             {/* Footer */}
             <footer className="fixed w-full bottom-0 p-4 bg-gray-800 text-white text-2xl">
-              <marquee>
-                Titas Gas Transmission & Distribution Company Limited
-              </marquee>
+              <marquee>{notice?.[0]?.text_en}</marquee>
             </footer>
           </div>
 
-
-
-          <div className="w-1/5 bg-gray-900 text-white h-full flex flex-col items-center">
-            <div className="w-full h-1/2 py-4 px-2 flex items-center justify-center mt-[100px]">
-              <div>
-                <h2 className="text-2xl text-center  font-bold mb-2">
-                  Notice Board
-                </h2>
-                <p className="text-lg text-center">{notice?.[0]?.text_en}</p>
-              </div>
-            </div>
-
-            <div className="w-full h-1/2 mt-[70px]">
+          <div className="w-1/5 bg-gray-900 text-white h-full flex flex-col items-center ">
+            <div className="w-full h-1/2  mt-[20px]">
               <Slider {...viddeoSettings} className="h-full">
                 {tvcList?.map((item) => (
                   <div key={item?.id} className="h-full w-full">
@@ -212,9 +226,37 @@ export default function CustomSlider() {
                 ))}
               </Slider>
             </div>
+            <div className="w-full flex-1  mt-[10px]">
+              <Slider {...viddeoSettings} className="h-full">
+                {tvcList?.map((item) => (
+                  <div key={item?.id} className="h-full w-full">
+                    <video
+                      className="h-full w-full object-cover"
+                      autoPlay
+                      muted
+                      loop
+                      playsInline
+                      preload="auto"
+                    >
+                      <source src={item?.url} type="video/mp4" />
+                      Your browser does not support the video tag.
+                    </video>
+                  </div>
+                ))}
+              </Slider>
+            </div>
+
+            {/* <div className="w-full py-4 px-2 flex items-center justify-center">
+              <div>
+                <h2 className="text-2xl text-center  font-bold mb-2">
+                  Notice Board
+                </h2>
+                <p className="text-sm text-center">{notice?.[0]?.text_en}</p>
+              </div>
+            </div> */}
           </div>
         </div>
       )}
     </div>
-  );
+  )
 }
