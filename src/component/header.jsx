86bc5a7429c76ef42f1657ef69657ec5462a478b
unknown <raidislam>
2024-10-09 01:44:38 +0600

    update

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      <div>
-        <h2>Logo</h2>
-        <h2>Titas Gas Transmission & Distribution Copany Limited</h2>
+    <div className="flex items-center gap-[120px]">
+      <div className="w-[180px]">
+        <img className="w-full" src="/images/shadow-image.svg" alt="" />
+      </div>
+      <div className="text-center">
+        <h2 className="text-[#202020] text-4xl font-bold tracking-wider mb-2">
+          Titas Gas Transmission & Distribution PLC.
+        </h2>
+        <p className="text-[#1CC475] text-3xl">
+          General Manager/Deputy Managing Director (DMD)
+        </p>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      flexGrow: {
+        2: "2", // Add flex-grow 2
+      },
+    },
   },
   plugins: [
     // ...
     flowbite.plugin(),
   ],
-}
+};
