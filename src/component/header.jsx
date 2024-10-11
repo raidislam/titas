daba4551b0c5870255283d264806c7f6aec4a824
engrmukul <engrmukul@hotmail.com>
2024-10-12 03:00:45 +0600

    fixed

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -12,7 +12,7 @@ export default function Header({ company_name, title, index, setting }) {
       </div>
       <div className="text-center flex justify-center items-center flex-1">
         <div className="">
-          <h2 className="text-[#202020] text-[1.75vw] font-bold tracking-wider mb-2 font-poppins">
+          <h2 className="text-[#1d8ef2] text-[1.75vw] font-bold tracking-wider mb-2 font-poppins">
             {company_name || "Titas Gas Transmission & Distribution PLC."}
           </h2>
           <p style={{color: titleColor, textDecoration: "underline"}} className="text-[1.35vw] font-poppins">{title}</p>
